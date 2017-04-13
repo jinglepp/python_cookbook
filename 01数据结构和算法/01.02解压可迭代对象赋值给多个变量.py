ca88a0797b111b2ca2d54c660ea50144e07293be
jinglepp <jinglepp@qq.com>
2017-04-13 14:54:15 +0800

    增加注释

--- 01数据结构和算法/01.02解压可迭代对象赋值给多个变量.py
+++ 01数据结构和算法/01.02解压可迭代对象赋值给多个变量.py
@@ -13,6 +13,7 @@ def drop_first_last(grades):
 record = ('Dave', 'dave@example.com', '773-555-1212', '847-555-1212')
 name, email, *phone_numbers = record
 print(name, email, phone_numbers)
+# Dave dave@example.com ['773-555-1212', '847-555-1212']
 
 sales_record = [11, 22, 33, 44]
 *trailing_qtrs, current_qtr = sales_record
